e2d4397e27b85fdc5c50363b92bf3ba9ca8a8a30
sirnicson <sirnicson.nicson@gmail.com>
2024-11-19 00:03:19 +0300

    Define BaseModel class

--- models/base_model.py
+++ models/base_model.py
@@ -1,12 +1,28 @@
 #!/usr/bin/python3
-"""
-BaseModel class to be inherited by other models.
-"""
+"""The BaseModel class nBnB"""
+import uuid
+from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self, id=None):
-        """
-        Initializes a new BaseModel instance with an optional ID.
-        """
-        self.id = id
+    def __init__(self):
+        """Initialize a new BaseModel Instance"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Return string representation of BaseModel instance"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        """Update the 'update_at' attribute to the current datetime"""
+        self.update_at = datetime.now()
+
+    def to_dict(self):
+        """Return a dictionary containing all instance attributes"""
+        result = self.__dict__.copy()
+        result["__class__"] = self.__class__.__name__
+        result["created_at"] = self.created_at.isoformat()
+        result["updated_at"] = self.updated_at.isoformat()
+        return result
